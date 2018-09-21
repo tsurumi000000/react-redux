eb688a2866bb501cca04657862560731267d311a
鶴見侑樹 <tsurumi0302@gmail.com>
2018-09-21 13:34:37 +0900

    learn componets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">aaaa</label>
-        <input type="text" onChange={() => { console.log("hi!!") }}></input>
-      </React.Fragment>
-    );
-  }
+// クラス Component
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">aaaa</label>
+//         <input type="text" onChange={() => { console.log("hi!!") }}></input>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+
+// 関数component
+const App = () => {
+  return (
+    <div>
+      Hi!!
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>aaaaaa</div>
 }
 
 export default App;
