7339eb7d3022e9f5494f6a0429f391cb37fa5e11
鶴見侑樹 <tsurumi0302@gmail.com>
2018-10-12 18:52:06 +0900

    events index

---
+++ src/components/events_index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { readEvents } from '../actions'
+import _ from 'lodash'
+
+// 関数component
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+// State情報をPropsとして扱うことができるということ。
+const mapStateToProps = state => ({ events: state.events })
+
+// Action関数をPropsとして扱うことができるということ。
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+
+const mapDispatchToProps = ({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
